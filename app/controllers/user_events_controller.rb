05abe1f9d391718bfe343ea6a7a531878756ad4b
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-08-01 12:47:18 -0400

    fixed code climate

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,9 +1,9 @@
 class UserEventsController < ApplicationController
 
   def create
-    @user_event = UserEvent.new(event_id: params[:event_id], grower_id: params[:grower_id])
+    @user_event = UserEvent.new(event_id: params[:event_id],
+      grower_id: params[:grower_id])
     @user_event.save
     redirect_to event_path(@user_event.event)
   end
-
 end
