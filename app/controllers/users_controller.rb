c87cc4080bf164cedf9b5c5fc174023e30baca27
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-08-01 14:31:59 -0400

    Added product/event/about details to homepage.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @events = UserEvent.where(grower_id: params[:id])
+    @products = Product.where(grower_id: params[:id])
   end
 end
