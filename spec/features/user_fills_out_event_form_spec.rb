8d007e7cbd6c601b71bbb9999cf3f8e4b3696dc0
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-07-31 21:27:43 -0400

    hopefully fixes codeship issue

--- spec/features/user_fills_out_event_form_spec.rb
+++ spec/features/user_fills_out_event_form_spec.rb
@@ -54,7 +54,7 @@ feature 'user adds a new event' do
     field = options[:from]
     select date.strftime('%Y'), :from => "#{field}_1i" #year
     select date.strftime('%B'), :from => "#{field}_2i" #month
-    select date.strftime('%d'), :from => "#{field}_3i" #day
+    select date.strftime('%-d'), :from => "#{field}_3i" #day
     select date.strftime('%I %p'), :from => "#{field}_4i" #hour
     select date.strftime('%M'), :from => "#{field}_5i" #minute
   end
