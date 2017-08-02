ff6e220d7638097375069a13c468f506fdab84cd
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-08-02 15:10:43 -0400

    Attending button fixed, plus button styling

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -6,4 +6,12 @@ class UserEventsController < ApplicationController
     @user_event.save
     redirect_to event_path(@user_event.event)
   end
+
+  def destroy
+    @user_event = UserEvent.find_by(event_id: params[:event_id],
+      grower_id: params[:grower_id])
+    if @user_event.destroy!
+      redirect_to event_path(Event.find(params[:event_id]))
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :products, only: [:new, :create]
   end
   resources :products, only: [:edit, :update, :destroy]
-  resources :user_events, only: [:create]
+  resources :user_events, only: [:create, :destroy]
 end
