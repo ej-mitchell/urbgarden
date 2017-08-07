7e1f280fd649eb71686b97ee14b8853a705daa0d
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-08-07 11:52:07 -0400

    j

--- app/javascript/react/src/containers/MapContainer.js
+++ app/javascript/react/src/containers/MapContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
+import styles from '../../../../assets/stylesheets/urbgarden.css';
 
 export class MapContainer extends React.Component {
   render () {
@@ -7,9 +8,9 @@ export class MapContainer extends React.Component {
       <Map
         google = { this.props.google }
         zoom = {14}
-        // style={{width: '100%', height: '100%'}}
-        className={'map'}
-        containerStyle={{height: '35%', width: '35%', margin: "200px"}}
+        style={{width: '100%', height: '100%'}}
+        className={style.map}
+        containerStyle={{height: '35%', width: '35%', margin: "200px", }}
         >
 
         <Marker
