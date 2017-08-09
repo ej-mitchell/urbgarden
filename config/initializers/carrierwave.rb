6f8f08b0cd3d83cfe7a2661583d1733052e86006
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-08-09 13:02:04 -0400

    Added AWS S3 for cloud storage of profile photos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,14 @@
 CarrierWave.configure do |config|
-  config.storage = :file
+  if !Rails.env.test?
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'us-east-1'
+    }
+    config.fog_directory  = ENV["S3_BUCKET"]
+  else
+    config.storage = :file
+  end
   config.enable_processing = false
 end
