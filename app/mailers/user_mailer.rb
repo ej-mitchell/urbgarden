5ea52f73be604cc9735ac6963551f679f707e0cb
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-08-10 13:39:37 -0400

    Basic ActionMailer functionality

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,12 @@
+require 'mailgun'
+
 class UserMailer < ApplicationMailer
 
   def event_email(user)
     @user = user
     @url  = 'https://urbgarden.herokuapp.com/users/sign_in'
-    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
-    message_params = {:from    => "urbgarden@example.com",
-                      :to      => @user.email,
-                      :subject => 'Sample Mail using Mailgun API',
-                      :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
-    mg_client.send_message ENV['domain'], message_params
+
+    mail to: @user.email, subject: "Success! You did it."
+    
   end
 end
