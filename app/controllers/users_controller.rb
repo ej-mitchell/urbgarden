589aa664f6048750b528601dd6285d6a61034416
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2017-08-14 18:52:43 -0400

    fixed a smorgasbord of things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @events = UserEvent.where(grower_id: params[:id])
+    
+    @user_events = UserEvent.where(grower_id: params[:id])
+    @events = []
+    @user_events.each do |event|
+      @events << event.event
+    end
+    
+    @user_created = Event.where(user_id: params[:id])
+    @user_created.each do |event|
+      @events << event
+    end
+    
     @products = Product.where(grower_id: params[:id])
   end
 end
